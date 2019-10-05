1db3301893a8589a4df48d1597f4f2f8dfde8068
Noah <n-k@live.ca>
2019-10-05 23:20:38 +0200

    add subtitle

--- studio/schemas/documents/siteSettings.js
+++ studio/schemas/documents/siteSettings.js
@@ -10,6 +10,12 @@ export default {
       title: 'Title'
     },
     {
+      name: 'subtitle',
+      type: 'string',
+      title: 'Subtitle',
+      description: 'Keep it short and simple'
+    },
+    {
       name: 'description',
       type: 'text',
       title: 'Description',
